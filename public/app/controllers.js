cfd53ae6ba415556a2a58c9a035fb7e998624aa0
Stephanie Barreyro <stephaniebarreyro@gmail.com>
2016-05-08 20:46:00 -0400

    implement threshold feature

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,6 +1,11 @@
 var schoolDashboard = angular.module('schoolDashboard', []);
 
 schoolDashboard.controller('StudentListCtrl', function($scope, $http) {
+  $scope.minAttendance = 0;
+  $scope.maxAttendance = 1;
+  $scope.thresholdFilter = function(student) {
+    return ( student.attendanceYtd > $scope.minAttendance && student.attendanceYtd < $scope.maxAttendance);
+  }
   $http.get('https://script.google.com/macros/s/AKfycbxtbtw8aL1oY3eyxjFE98kOCqdaG4T4pWvPbNcEv9PlWHPbHQF_/exec').success(function(data) {
     $scope.students = data;
   });
